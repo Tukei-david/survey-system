e440b1c283ed02cb58e6b4680c9c219c45793845
Tukei-david <dtukei831@gmail.com>
2023-01-30 17:21:40 +0300

    Adding sign up and in Controllers

--- api/database/migrations/2023_01_30_081835_create_surveys_table.php
+++ api/database/migrations/2023_01_30_081835_create_surveys_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->tinyInteger('status');
             $table->text('description')->nullable();
             $table->timestamps();
+            $table->timestamp('expire_date')->nullable();
         });
     }
 
