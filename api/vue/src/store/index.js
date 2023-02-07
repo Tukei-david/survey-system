01662b2b9119314d7dcca911effeba465b7ac97e
Tukei-david <dtukei831@gmail.com>
2023-02-07 23:15:07 +0300

    CRUD Front end

--- api/vue/src/store/index.js
+++ api/vue/src/store/index.js
@@ -151,6 +151,20 @@ const store = createStore({
     },
     getters: {},
     actions: {
+        saveSurvey({ commit }, survey) {
+            let response
+            if (survey.id) {
+                response = axiosClient.put(`/survey/${survey.id}`, survey).then((res) => {
+                    commit("updateSurvey", res.data)
+                    return res
+                })
+            } else {
+                response = axiosClient.post("/survey", survey).then((res) => {
+                    commit("saveSurvey", res.data)
+                    return res
+                })
+            }
+        },
         register ({ commit }, user) {
             return axiosClient.post('/register', user)
                 .then(({data}) => {
@@ -174,6 +188,17 @@ const store = createStore({
         }
     },
     mutations: {
+        saveSurvey: (state, survey) => {
+            state.surveys = [...state.surveys, survey.data]
+        },
+        updateSurvey: (state, survey) => {
+            state.surveys = state.surveys.map((s) => {
+                if (s.id == survey.data.id) {
+                    return survey.data
+                }
+                return s
+            })
+        },
         logout: (state) => {
             state.user.data = {}
             state.user.token = null
--- api/vue/src/views/SurveyView.vue
+++ api/vue/src/views/SurveyView.vue
@@ -187,11 +187,12 @@
 import { v4 as uuid4v4 } from "uuid"
 import store from "../store";
 import { ref } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import PageComponent from "../components/PageComponent.vue";
 import QuestionEditor from "../components/editor/QuestionEditor.vue";
 
 const route = useRoute();
+const router = useRouter()
 
 let model = ref({
     title: "",
@@ -233,6 +234,15 @@ function questionChange(question) {
             return JSON.parse(JSON.stringify(question))
         }
         return q
+    })
+}
+
+function saveSurvey() {
+    store.dispatch("saveSurvey", model.value).then(({ data }) => {
+        router.push({
+            name: "SurveyView",
+            params: { id: data.data.id }
+        })
     })
 }
 </script>
